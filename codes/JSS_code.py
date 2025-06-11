36269355ae8d9080b25aef071e947492d0c5aa14
Doruk Cengiz <dcdorukcengiz@gmail.com>
2025-06-11 11:27:10 -0400

    code with additional features and covariate adjustments

--- codes/JSS_code.py
+++ codes/JSS_code.py
@@ -41,9 +41,7 @@ data_prep = scdata(df = data, id_var = id_var, time_var = time_var,
  unit_co = unit_co, cointegrated_data = cointegrated_data,
  constant = constant)
 
-with io.capture_output() as captured:
-    with redirect_stdout(IO.StringIO()):
-        est_si = scest.scest(data_prep, w_constr={"name": "simplex"})
+est_si = scest.scest(data_prep, w_constr={"name": "simplex"})
 
 
 e_method = "gaussian"
@@ -66,18 +64,60 @@ e_lags = 0
 sims = 1000
 cores = 1
 
+#Lasso does not work for some reason: Estimation algorithm not converged! The algorithm returned the value: optimal_inaccurate
+#est_lasso2 = scest.scest(data_prep, w_constr={'p': 'L1', 'dir': '<=', 'Q': 1, 'lb': -np.inf})
 
-est_lasso2 = scest.scest(data_prep, w_constr={'p': 'L1', 'dir': '<=', 'Q': 1, 'lb': -np.inf})
-
-
-
-
-for mtd in ["ridge", ""]:
-    np.random.seed(8894)
-    pi_si = scpi(data_prep, sims = sims, w_constr = {"name": mtd},
+pi_si = scpi(data_prep, sims = sims, w_constr = {"name": 'L1-L2'},
     u_order = u_order, u_lags = u_lags, e_order = e_order,
     e_lags = e_lags, e_method = e_method, u_missp = u_missp,
     u_sigma = u_sigma, cores = cores)
+
+pi_si = scpi(data_prep, sims = sims, w_constr = {"name": 'ridge'},
+    u_order = u_order, u_lags = u_lags, e_order = e_order,
+    e_lags = e_lags, e_method = e_method, u_missp = u_missp,
+    u_sigma = u_sigma, cores = cores)
+
+pi_si = scpi(data_prep, sims = sims, w_constr = {"name": 'ols'},
+    u_order = u_order, u_lags = u_lags, e_order = e_order,
+    e_lags = e_lags, e_method = e_method, u_missp = u_missp,
+    u_sigma = u_sigma, cores = cores)
+
 plot = scplot(pi_si, x_lab = "Year", e_method = e_method,
     y_lab = "GDP per capita (thousand US dollars)")
 plot + ggtitle("")
+
+
+
+id_var = "country"
+outcome_var = "gdp"
+time_var = "year"
+period_pre = np.arange(1960, 1991)
+period_post = np.arange(1991, 2004)
+unit_tr = "West Germany"
+unit_co = list(set(data[id_var].to_list()))
+unit_co = [cou for cou in unit_co if cou != "West Germany"]
+constant = False
+cointegrated_data = True
+cov_adj = [["constant"], ["constant", "trend"]]
+
+
+data_prep = scdata(df = data, id_var = id_var, time_var = time_var,
+    outcome_var = outcome_var, period_pre = period_pre,
+    period_post = period_post, unit_tr = unit_tr,
+    unit_co = unit_co, cointegrated_data = cointegrated_data,
+    constant = constant, features = ["gdp", "trade"], 
+    cov_adj = cov_adj)
+
+est_si = scest.scest(data_prep, w_constr = {"name": "simplex"})
+weights_and_constant = est_si.b.reset_index()
+
+weights_and_constant = weights_and_constant.rename(columns = {0: "weight"})
+weights_and_constant.query("~donor.str.contains('constant')")["weight"].sum()
+
+
+plot = scplot(est_si, x_lab = "Year", e_method = e_method,
+    y_lab = "GDP per capita (thousand US dollars)")
+plot + ggtitle("")
+
+
+# %%
